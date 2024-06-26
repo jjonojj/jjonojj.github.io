3b3c0c163cfb2198497dc2c5df632c4a69763933
jjonojj <jo.piechl@outlook.com>
2024-06-26 17:34:19 +0200

    cool async stuff wohoo

---
+++ main.js
@@ -0,0 +1,23 @@
+let scr = document.getElementById("scr");
+
+let items = [];
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+for (let i = 0; i <= 127; i++) {
+  items.push(String.fromCharCode(i));
+}
+
+async function rchars() {
+    scr.innerText = ""
+    for (let i = 0; i < 7; i++) {
+        add();
+        await sleep(30);
+    }
+}
+
+function add() {
+    scr.innerText += items[Math.floor(Math.random()*items.length)];
+}
+
+setInterval(rchars, 1000);
